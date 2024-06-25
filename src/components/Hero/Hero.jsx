af01f61f1cfc10432ddf53cd96ca5bb9be7ff94c
bitwisebro <34713448+bitwisebro@users.noreply.github.com>
2024-06-25 19:43:27 +0530

    delete dist

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -9,14 +9,17 @@ export const Hero = () => {
       <div className={styles.content}>
         <h1 className={styles.title}>Hi, I'm Gaurav Jani</h1>
         <p className={styles.description}>
-          I'm a Software Engineer with 2+ years of experience in QA. Reach out if you'd like to learn more!
+          I'm a Software Engineer with 2+ years of experience in QA. Reach out
+          if you'd like to learn more!
         </p>
         <a href="mailto:gauravjani207@yahoo.com" className={styles.contactBtn}>
           Contact Me
         </a>
       </div>
       <img
-        src={getImageUrl("hero/heroImage.png")}
+        src={getImageUrl(
+          "https://img.freepik.com/free-vector/man-using-laptop-cartoon_1308-120757.jpg"
+        )}
         alt="Hero image of me"
         className={styles.heroImg}
       />
